bc64312665b566f19e7ce963085afab7c6542d7a
Adwait Rawat <adwait.rawat@gmail.com>
2020-09-14 12:33:41 +0900

    Add StationPath class to marsPath.hpp

--- src/marsPath.hpp
+++ src/marsPath.hpp
@@ -9,6 +9,15 @@ namespace MARS
     constexpr static char MAIN[] = "Fare_Data/(H)Main_Line_Fare.csv";
     constexpr static char BRANCH[] = "Fare_Data/(H)Branch_Line_Fare.csv";
   };
+
+  class StationPath
+  {
+  public:
+    constexpr static char SASSHO_LINE[] = "Station_Data/JR_Hokkaido/(H)Sassho_Line.csv";
+    constexpr static char SEKIHOKU_LINE[] = "Station_Data/JR_Hokkaido/(H)Sekihoku_Line.csv";
+    constexpr static char SEKISHO_LINE[] = "Station_Data/JR_Hokkaido/(H)Sekisho_Line.csv";
+    constexpr static char SENMO_LINE[] = "Station_Data/JR_Hokkaido/(H)Senmo_Line.csv";
+  };
 }
 
 #endif  /* end of marsPath.hpp */
