142a0f6a7dd9f73c67a08447b4a4a2a084bcd0f2
Adwait Rawat <adwait.rawat@gmail.com>
2020-09-14 12:28:55 +0900

    Add FarePath class to MARS namespace
    All Paths will be stored in marsPath.hpp

---
+++ src/marsPath.hpp
@@ -0,0 +1,14 @@
+#include <string>
+#ifndef MARS_PATH_HPP
+
+namespace MARS
+{
+  class FarePath
+  {
+  public:
+    constexpr static char MAIN[] = "Fare_Data/(H)Main_Line_Fare.csv";
+    constexpr static char BRANCH[] = "Fare_Data/(H)Branch_Line_Fare.csv";
+  };
+}
+
+#endif  /* end of marsPath.hpp */
